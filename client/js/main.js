419f5a543af5c6a206196a24aa037945127bbf1d
Premysl Bednarek <premik09@seznam.cz>
2024-01-17 12:04:23 +0100

    resize canvas on window resize

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,16 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+
+function resizeCanvas() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix(); // must be called after changing camera's properties
+}
+
+resizeCanvas();
+
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -19,3 +27,6 @@ function animate() {
     cube.rotation.y += 0.01;
 }
 animate();
+
+// resize the canvas when the windows size changes
+window.addEventListener('resize', resizeCanvas, false);

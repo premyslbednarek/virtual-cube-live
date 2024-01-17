4e1f753e82199af93ff59f89d0d1076360179255
Premysl Bednarek <premik09@seznam.cz>
2024-01-17 15:16:43 +0100

    add orbitcontrols

--- client/js/main.js
+++ client/js/main.js
@@ -3,6 +3,7 @@ import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial
 
 const scene = new Scene();
 let camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(3,3,3)
 
 const renderer = new WebGLRenderer();
 
@@ -14,6 +15,16 @@ function resizeCanvas() {
 
 resizeCanvas();
 
+const controls = new OrbitControls(camera, renderer.domElement);
+//controls.update() must be called after any manual changes to the camera's transform
+// camera.position.set( 0, 20, 100 );
+controls.minDistance = 2;
+controls.maxDistance = 100;
+controls.enableZoom = false;
+controls.enablePan = false; // disable moving the camera with right click
+controls.update();
+
+
 document.body.appendChild( renderer.domElement );
 
 const geometry = new BoxGeometry( 1, 1, 1 );
@@ -21,13 +32,17 @@ const material = new MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+// camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+	// controls.update();
 }
 animate();
 

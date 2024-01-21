033dbcbf0a88f02ecc0d5d8a7e7b4cb2423101fc
Premysl Bednarek <premik09@seznam.cz>
2024-01-21 11:29:10 +0100

    3x3 cube is turnable by mouse inputs, 4x4 is todo

--- client/js/main.js
+++ client/js/main.js
@@ -80,6 +80,7 @@ function getClickedAxis(pos) {
 
 function onMouseDown( event ) {
     cleanGroup();
+    axisMovements = undefined;
     // console.log("Mouse down coordinates:", event.clientX, event.clientY);
 	// calculate pointer position in normalized device coordinates
 	// (-1 to +1) for both components
@@ -95,41 +96,49 @@ function onMouseDown( event ) {
     // console.log("Number of objects intersected:", intersects.length);
 
     if (intersects.length && intersects[0].object.isSticker) {
-        // controls.enabled = false; // disable rotating camera
-        axisMovements = [];
+        controls.enabled = false; // disable rotating camera
         var pos = intersects[0].object.position;
-        console.log("clicked stijijiijijcker position", pos.x, pos.y, pos.z)
+        // console.log("clicked stijijiijijcker position", pos.x, pos.y, pos.z)
         var clickedAxis = getClickedAxis(pos);
         window.pos = pos;
         var sign = pos[clickedAxis] / Math.abs(pos[clickedAxis]);
-        console.log(clickedAxis, sign);
+        // console.log(clickedAxis, sign);
         var pointA = intersects[0].point;
+        var v1 = new THREE.Vector3(0, 0, 0);
+        var v2 = new THREE.Vector3(0, 0, 0);
         if (clickedAxis == "x") {
             if (sign > 0) {
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, 0, 1)));
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, -1, 0)));
+                v1.z = 1;
+                v2.y = -1;
             } else {
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, 0, -1)));
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, 1, 0)));
+                v1.z = -1;
+                v2.y = 1;
             }
         } else if (clickedAxis == "y") {
             if (sign > 0) {
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, 0, -1)));
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(1, 0, 0)));
+                v1.z = -1;
+                v2.x = 1;
             } else {
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, 0, 1)));
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(-1, 0, 0)));
+                v1.z = 1;
+                v2.x = -1;
             }
         } else {
             if (sign > 0) {
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, 1, 0)));
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(-1, 0, 0)));
+                v1.y = 1;
+                v2.x = -1;
             } else {
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(0, -1, 0)));
-                drawLine(pointA, pointA.clone().add(new THREE.Vector3(1, 0, 0)));
+                v1.y = -1;
+                v2.x = 1;
             }
         }
-
+        // console.log(v1, v2);
+        // drawLine(pointA, pointA.clone().add(v1));
+        // drawLine(pointA, pointA.clone().add(v2));
+        var v1_neg = v1.clone().multiplyScalar(-1);
+        var v2_neg = v2.clone().multiplyScalar(-1);
+        // drawLine(pointA, pointA.clone().add(v1_neg));
+        // drawLine(pointA, pointA.clone().add(v2_neg));
+        axisMovements = {clickedAxis: clickedAxis, clickedPosition: intersects[0].point, stickerPosition: pos, vectors: [v1, v2, v1_neg, v2_neg] };
 
         // if (Math.abs(1.501 - pointA.x) > 0.1) {
         //     var pointB = pointA.clone().add(new THREE.Vector3(1, 0, 0))
@@ -156,7 +165,11 @@ function onMouseDown( event ) {
 window.addEventListener('mousedown', onMouseDown);
 
 function onMouseUp(event) {
-    if (axisMovements.length == 0) return;
+    // enable controls, that were disable on mouse down
+    controls.enabled = true;
+    // no sticker was clicked
+    if (axisMovements == undefined) return;
+
     var x = event.clientX;
     var y = event.clientY;
 	// var x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -168,33 +181,60 @@ function onMouseUp(event) {
         axisMovements = [];
         return;
     }
-    console.log("Mouse vector", movementVector);
+
+    // console.log("Mouse vector", movementVector);
     var angles = []
-    for (var i = 0; i < 2; ++i) {
-        var vectorS = axisMovements[i][1].clone();
+    for (var vector of axisMovements.vectors) {
+        var vectorS = axisMovements.clickedPosition.clone();
         vectorS.project(camera);
         vectorS.x = ( vectorS.x + 1) * window.innerWidth / 2;
         vectorS.y = - ( vectorS.y - 1) * window.innerHeight / 2;
         vectorS.z = 0;
         // console.log("S", vectorS);
-        var vectorE = axisMovements[i][2].clone();
+        var vectorE = axisMovements.clickedPosition.clone().add(vector);
         vectorE.project(camera);
         vectorE.x = ( vectorE.x + 1) * window.innerWidth / 2;
         vectorE.y = - ( vectorE.y - 1) * window.innerHeight / 2;
         vectorE.z = 0;
         vectorE.sub(vectorS)
+
         var angle = movementVector.angleTo(vectorE);
-        console.log(`Vektor #${i + 1}`, vectorE);
-        console.log(`Angle  #${i + 1}`, angle*57);
-        angles.push([axisMovements[i][0], angle]);
+        // console.log(`Vektor`, angle*57, vectorE);
+        angles.push(angle);
     }
-    console.log(angles)
-    if (angles[0][1] < angles[1][1]) {
-        console.log("MoveA");
-        rotateGroupGen((_) => true, angles[1][0], 1);
-    } else {
-        console.log("MoveB");
-        rotateGroupGen((_) => true, angles[0][0], 1);
+
+    var lowestAngleIndex = 0;
+    var lowestAngle = angles[0];
+    for (var i = 1; i < angles.length; ++i) {
+        if (angles[i] < lowestAngle) {
+            lowestAngle = angles[i];
+            lowestAngleIndex = i;
+        }
+    }
+    var bestVector = axisMovements.vectors[lowestAngleIndex];
+    console.log("Lowest angle is: ", lowestAngle*57, "to vector", axisMovements.vectors[lowestAngleIndex]);
+    console.log(axisMovements.clickedAxis)
+    var sign = axisMovements.stickerPosition[axisMovements.clickedAxis] / Math.abs(axisMovements.stickerPosition[axisMovements.clickedAxis]);
+    if (axisMovements.clickedAxis == "x") {
+        if (bestVector.y != 0) {
+            rotateGroupGen((obj) => Math.abs(obj.position.z - axisMovements.stickerPosition.z) < 0.75,"z", sign*bestVector.y / Math.abs(bestVector.y));
+        } else {
+            rotateGroupGen((obj) => Math.abs(obj.position.y - axisMovements.stickerPosition.y) < 0.75,"y", sign*-bestVector.z / Math.abs(bestVector.z));
+        }
+    }
+    if (axisMovements.clickedAxis == "y") {
+        if (bestVector.x != 0) {
+            rotateGroupGen((obj) => Math.abs(obj.position.z - axisMovements.stickerPosition.z) < 0.75,"z", sign*-bestVector.x / Math.abs(bestVector.x));
+        } else {
+            rotateGroupGen((obj) => Math.abs(obj.position.x - axisMovements.stickerPosition.x) < 0.75,"x", sign*bestVector.z / Math.abs(bestVector.z));
+        }
+    }
+    if (axisMovements.clickedAxis == "z") {
+        if (bestVector.x != 0) {
+            rotateGroupGen((obj) => Math.abs(obj.position.y - axisMovements.stickerPosition.y) < 0.75,"y", sign*bestVector.x / Math.abs(bestVector.x));
+        } else {
+            rotateGroupGen((obj) => Math.abs(obj.position.x - axisMovements.stickerPosition.x) < 0.75,"x", sign*-bestVector.y / Math.abs(bestVector.y));
+        }
     }
 
     // console.log("Angles", angles);
@@ -224,7 +264,7 @@ function onMouseUp(event) {
 }
 window.addEventListener('mouseup', onMouseUp);
 
-const fps = 10;
+const fps = 30;
 async function animate() {
 	renderer.render( scene, camera );
     TWEEN.update();
@@ -260,6 +300,7 @@ function cleanGroup() {
 window.cleanGroup = cleanGroup;
 
 function rotateGroupGen(checkFunction, axis, mult) {
+    console.log("rotation started", checkFunction, axis, mult);
     if (tween && tween.isPlaying()) {
         tween.end();
         cleanGroup();

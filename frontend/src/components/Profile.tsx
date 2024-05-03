429ddc96f514a58887dab6a56474a91d47b01641
Premysl Bednarek <premik09@seznam.cz>
2024-05-03 17:08:47 +0200

    fix profile for anonymous users

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -5,10 +5,6 @@ import { User } from "./UserPage";
 export default function Profile() {
     const { userContext } = useContext(UserContext)
 
-    if (!userContext.isLogged) {
-        return <div>You are not logged in!</div>;
-    }
-
     return (
         <User username={userContext.username} />
     );

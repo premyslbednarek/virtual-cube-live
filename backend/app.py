a1baa64b8ea59e000aac531131addd2379ff601b
Premysl Bednarek <premik09@seznam.cz>
2024-05-10 17:22:34 +0200

    be able to join to docker db in local development

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 conn_url = f"postgresql://" \
            f"{os.environ.get('DB_USERNAME')}" \
            f":{os.environ.get('DB_PASSWORD')}" \
-           f"@db:{os.environ.get('DB_PORT')}" \
+           f"@{os.environ.get('SOURCE', 'db')}:{os.environ.get('DB_PORT')}" \
            f"/{os.environ.get('DB_NAME')}"
 
 app = Flask(__name__)
